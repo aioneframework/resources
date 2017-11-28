956b04d541612b96619671a34da1add310a76cf0
SGS Sandhu <sgs.sandhu@gmail.com>
2017-11-28 11:34:38 +0530

    Update 20171128060438

---
+++ images/index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Aione Framework</title>
+	<!-- Global stylesheets -->
+	<link rel="stylesheet" type="text/css" href="http://aioneframework.com/aioneframework/assets/css/aione.min.css">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:100,300,400,600,700,800,900">
+	<script type="text/javascript" src="http://aioneframework.com/aioneframework/assets/js/vendor.min.js"></script>
+	<script type="text/javascript" src="http://aioneframework.com/aioneframework/assets/js/aione.min.js"></script>
+
+</head>
+<body class="">
+	<div id="aione_wrapper" class="aione-wrapper aione-layout-wide aione-theme-arcane">
+		<div class="aione-row">
+			<div id="aione_header" class="aione-header">
+				<div class="aione-row">
+					
+
+				</div><!-- .aione-row -->
+			</div><!-- #aione_header -->
+			<div id="aione_main" class="aione-main fullwidth">
+				<div class="aione-row">
+					<div id="aione_content" class="aione-content">
+						<div class="aione-row">
+
+							<div id="aione_page_header" class="aione-page-header">
+								<div class="aione-row">
+									<div class="aione-page-title-container">
+										<h3 class="m-0">Aione Gallery</h3>
+										<div class="clear"></div>
+									</div> <!-- .aione-page-title-container -->
+									<div class="clear"></div>
+								</div> <!-- .row -->
+							</div> <!-- #aione_page_header --> 
+
+							<div id="aione_page_content" class="">
+								<div class="aione-row">
+									<div class="ar pv-20 ph-50">
+									<div class="ac s100 m100 l100">
+										<!-- .allery -->
+										<div class="gallery outline margin square animation-zoom gallery-columns-4 ">
+											<!-- .gallery-item -->
+											<div class="gallery-item ">
+												<div class="gallery-icon">
+													<img src="http://aioneframework.com/resources/images/gallery/thumb/gallery-1.jpg" data-src="http://aioneframework.com/resources/images/gallery/gallery-1.jpg"/>
+												</div>
+												<div class="gallery-caption">
+													<h3 class="mt-30 white">Image Title </h3>
+													<p>Image Description</p>
+												</div>
+
+											</div>
+											<!-- .gallery-item -->
+											<div class="gallery-item">
+												<div class="gallery-icon">
+													<img src="http://aioneframework.com/resources/images/gallery/thumb/gallery-1.jpg" data-src="http://aioneframework.com/resources/images/gallery/gallery-1.jpg"/>
+												</div>
+												<div class="gallery-caption">
+													<h3 class="mt-30 white">Image Title </h3>
+													<p>Image Description</p>
+												</div>
+											</div>
+											<!-- .gallery-item -->
+											<div class="gallery-item">
+												<div class="gallery-icon">
+													<img src="http://aioneframework.com/resources/images/gallery/thumb/gallery-2.jpg" data-src="http://aioneframework.com/resources/images/gallery/gallery-2.jpg"/>
+												</div>
+												<div class="gallery-caption">
+													<h3 class="mt-30 white">Image Title </h3>
+													<p>Image Description</p>
+												</div>
+											</div>
+											<!-- .gallery-item -->
+											<div class="gallery-item">
+												<div class="gallery-icon">
+													<img src="http://aioneframework.com/resources/images/gallery/thumb/gallery-3.jpg" data-src="http://aioneframework.com/resources/images/gallery/gallery-3.jpg"/>
+												</div>
+												<div class="gallery-caption">
+													<h3 class="mt-30 white">Image Title </h3>
+													<p>Image Description</p>
+												</div>
+											</div>
+											<!-- .gallery-item -->
+											<div class="gallery-item">
+												<div class="gallery-icon">
+													<img src="http://aioneframework.com/resources/images/gallery/thumb/gallery-4.jpg" data-src="http://aioneframework.com/resources/images/gallery/gallery-4.jpg"/>
+												</div>
+												<div class="gallery-caption">
+													<h3 class="mt-30 white">Image Title </h3>
+													<p>Image Description</p>
+												</div>
+											</div>
+											<!-- .gallery-item -->
+											<div class="gallery-item">
+												<div class="gallery-icon">
+													<img src="http://aioneframework.com/resources/images/gallery/thumb/gallery-5.jpg" data-src="http://aioneframework.com/resources/images/gallery/gallery-5.jpg"/>
+												</div>
+												<div class="gallery-caption">
+													<h3 class="mt-30 white">Image Title </h3>
+													<p>Image Description</p>
+												</div>
+											</div>
+											<!-- .gallery-item -->
+											<div class="gallery-item">
+												<div class="gallery-icon">
+													<img src="http://aioneframework.com/resources/images/gallery/thumb/gallery-6.jpg" data-src="http://aioneframework.com/resources/images/gallery/gallery-6.jpg"/>
+												</div>
+												<div class="gallery-caption">
+													<h3 class="mt-30 white">Image Title </h3>
+													<p>Image Description</p>
+												</div>
+											</div>
+											<!-- .gallery-item -->
+											<div class="gallery-item">
+												<div class="gallery-icon">
+													<img src="http://aioneframework.com/resources/images/gallery/thumb/gallery-7.jpg" data-src="http://aioneframework.com/resources/images/gallery/gallery-17.jpg"/>
+												</div>
+												<div class="gallery-caption">
+													<h3 class="mt-30 white">Image Title </h3>
+													<p>Image Description</p>
+												</div>
+											</div>
+											<!-- .gallery-item -->
+											<div class="gallery-item">
+												<div class="gallery-icon">
+													<img src="http://aioneframework.com/resources/images/gallery/thumb/gallery-8.jpg" data-src="http://aioneframework.com/resources/images/gallery/gallery-8.jpg"/>
+												</div>
+												<div class="gallery-caption">
+													<h3 class="mt-30 white">Image Title </h3>
+													<p>Image Description</p>
+												</div>
+											</div>
+											<!-- .gallery-item -->
+											<div class="gallery-item">
+												<div class="gallery-icon">
+													<img src="http://aioneframework.com/resources/images/gallery/thumb/gallery-9.jpg" data-src="http://aioneframework.com/resources/images/gallery/gallery-9.jpg"/>
+												</div>
+												<div class="gallery-caption">
+													<h3 class="mt-30 white">Image Title </h3>
+													<p>Image Description</p>
+												</div>
+											</div>
+											<!-- .gallery-item -->
+											<div class="gallery-item">
+												<div class="gallery-icon">
+													<img src="http://aioneframework.com/resources/images/gallery/thumb/gallery-10.jpg" data-src="http://aioneframework.com/resources/images/gallery/gallery-10.jpg"/>
+												</div>
+												<div class="gallery-caption">
+													<h3 class="mt-30 white">Image Title </h3>
+													<p>Image Description</p>
+												</div>
+											</div>
+											<!-- .gallery-item -->
+											<div class="gallery-item">
+												<div class="gallery-icon">
+													<img src="http://aioneframework.com/resources/images/gallery/thumb/gallery-11.jpg" data-src="http://aioneframework.com/resources/images/gallery/gallery-11.jpg"/>
+												</div>
+												<div class="gallery-caption">
+													<h3 class="mt-30 white">Image Title </h3>
+													<p>Image Description</p>
+												</div>
+											</div>
+											<!-- .gallery-item -->
+											<div class="gallery-item">
+												<div class="gallery-icon">
+													<img src="http://aioneframework.com/resources/images/gallery/thumb/gallery-12.jpg" data-src="http://aioneframework.com/resources/images/gallery/gallery-12.jpg"/>
+												</div>
+												<div class="gallery-caption">
+													<h3 class="mt-30 white">Image Title </h3>
+													<p>Image Description</p>
+												</div>
+											</div>
+											<!-- .gallery-item -->
+											<div class="gallery-item">
+												<div class="gallery-icon">
+													<img src="http://aioneframework.com/resources/images/gallery/thumb/gallery-13.jpg" data-src="http://aioneframework.com/resources/images/gallery/gallery-13.jpg"/>
+												</div>
+												<div class="gallery-caption">
+													<h3 class="mt-30 white">Image Title </h3>
+													<p>Image Description</p>
+												</div>
+											</div>
+											<!-- .gallery-item -->
+											<div class="gallery-item">
+												<div class="gallery-icon">
+													<img src="http://aioneframework.com/resources/images/gallery/thumb/gallery-14.jpg" data-src="http://aioneframework.com/resources/images/gallery/gallery-14.jpg"/>
+												</div>
+												<div class="gallery-caption">
+													<h3 class="mt-30 white">Image Title </h3>
+													<p>Image Description</p>
+												</div>
+											</div>
+											<!-- .gallery-item -->
+											<div class="gallery-item">
+												<div class="gallery-icon">
+													<img src="http://aioneframework.com/resources/images/gallery/thumb/gallery-15.jpg" data-src="http://aioneframework.com/resources/images/gallery/gallery-15.jpg"/>
+												</div>
+												<div class="gallery-caption">
+													<h3 class="mt-30 white">Image Title </h3>
+													<p>Image Description</p>
+												</div>
+											</div>
+											<!-- .gallery-item -->
+											<div class="gallery-item">
+												<div class="gallery-icon">
+													<img src="http://aioneframework.com/resources/images/gallery/thumb/gallery-16.jpg" data-src="http://aioneframework.com/resources/images/gallery/gallery-16.jpg"/>
+												</div>
+												<div class="gallery-caption">
+													<h3 class="mt-30 white">Image Title </h3>
+													<p>Image Description</p>
+												</div>
+											</div>
+											
+										</div>
+										<!-- .gallery -->
+
+									</div>
+									</div>
+									
+									<!-- .aione-row -->
+							</div> <!-- #aione_page_content -->
+						</div><!-- .aione-row -->
+					</div><!-- #aione_content -->
+					<div class="clear"></div><!-- .clear -->
+				</div><!-- .aione-row -->
+			</div><!-- #aione_main -->
+			<div id="aione_footer" class="aione-footer">
+				<div class="aione-row">
+					&copy;2017 <a href="http://oxosolutions.com/" target="_blank">OXO Solutions</a>. All rights reserved. 
+				</div><!-- .aione-row -->
+			</div><!-- #aione_content --> 
+			<div class="clear"></div><!-- .clear -->
+		</div><!-- .aione-row -->
+	</div><!-- #aione_wrapper -->
+</body>
+</html>
